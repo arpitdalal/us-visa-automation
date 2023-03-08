e658402e3d5bd25053b1e009ca1c72ccf71d1a0c
arpitdalal <arpitdalalm@gmail.com>
2023-03-08 16:29:49 -0500

    🩹 try to make it work on netlify functions

--- netlify/functions/us-visa-automation/index.ts
+++ netlify/functions/us-visa-automation/index.ts
@@ -38,7 +38,7 @@ const getBrowser = async () => {
     return puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(),
+      executablePath: await chromium.executablePath("/var/task/bin/"),
       headless: chromium.headless,
       ignoreHTTPSErrors: true,
     });
